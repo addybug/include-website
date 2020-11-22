e36668326c9de2f4b4ac2b651966396485afad03
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-11-22 01:31:52 -0600

    finished crash course w/ live lesson ejs

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ module.exports = function(app) {
         topics: "<li>Python introduction</li> <li>Data types</li> <li>Variables</li> <li>Strings</li>",
         title: "Song Lyrics",
         description: "<p> Find your favorite song on Genius and print the chorus line by line in Python. </p> <ul> <li> Hint: use <span class='function-style'>print</span>() and <span class='var-style'>/n</span> </li> </ul> <h2 class='res_eve_des project-header' > Ice Breaker 1 </h2> <p> Create variables (e.g. “<span style='color: #333333; font-weight: bold' >name</span>”, “<span class='var-style'>age</span>”) and set their values to the corresponding fact about yourself. Then, print the variable. Create at least 10 variables and use proper Python naming conventions. </p>",
+        slideShow: "https://docs.google.com/presentation/d/1g6oshn4ALGeCruvrAJfxwfFVMzuUI9PwasGhNUmhOJM/edit?usp=sharing",
+        youtube: "https://youtu.be/a8IVRrz1DOc"
       }
     }
       res.render('pages/about', {info: crashContent[req.params.day]});
