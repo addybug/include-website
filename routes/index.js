32528aaa838c08a88ae0c1c543a9cadec76e16d1
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-11-22 01:21:58 -0600

    switching to ejs (june 13)

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function(app) {
+
+  app.get('/crash-course/live-lesson/:day', function(req, res) {
+    let crashContent = {
+      june13: {
+        day: "Saturday June 13",
+        time: "Time: 10am-12pm CST",
+        topics: "<li>Python introduction</li> <li>Data types</li> <li>Variables</li> <li>Strings</li>",
+        title: "Song Lyrics",
+        description: "<p> Find your favorite song on Genius and print the chorus line by line in Python. </p> <ul> <li> Hint: use <span class='function-style'>print</span>() and <span class='var-style'>/n</span> </li> </ul> <h2 class='res_eve_des project-header' > Ice Breaker 1 </h2> <p> Create variables (e.g. “<span style='color: #333333; font-weight: bold' >name</span>”, “<span class='var-style'>age</span>”) and set their values to the corresponding fact about yourself. Then, print the variable. Create at least 10 variables and use proper Python naming conventions. </p>",
+      }
+    }
+      res.render('pages/about', {info: crashContent[req.params.day]});
+  });
+};
