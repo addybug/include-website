03d130a72f615c4a63a83288192086b407a21a99
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-11-25 23:24:02 -0600

    fixed names faq

--- views/js/faq.js
+++ views/js/faq.js
@@ -1,52 +1,41 @@
 var clics = 0;
 
 $(document).ready(function() {
-  $(".respuesta").hide();
-  $("#cerrartodas").hide();
+  $(".question").hide();
+  $("#closeAll").hide();
 
   $("h3.faqq").click(function() {
     $(this)
-      .next(".respuesta")
+      .next(".question")
       .toggle(
         function() {
-          $(this).next(".respuesta");
+          $(this).next(".question");
         },
         function() {
           $(this)
-            .next(".respuesta")
+            .next(".question")
             .fadeIn("fast");
         }
       );
 
-    if ($(this).hasClass("cerrar")) {
-      $(this).removeClass("cerrar");
+    if ($(this).hasClass("closed")) {
+      $(this).removeClass("closed");
     } else {
-      $(this).addClass("cerrar");
+      $(this).addClass("closed");
     }
 
-    if ($(".cerrar").length >= 3) {
-      $("#cerrartodas").fadeIn("fast");
+    if ($(".closed").length >= 3) {
+      $("#closeAll").fadeIn("fast");
     } else {
-      $("#cerrartodas").hide();
-      var abiertas = $(".cerrar").length;
+      $("#closeAll").hide();
+      var abiertas = $(".closed").length;
       console.log(abiertas);
     }
   }); //Close Function Click
 }); //Close Function Ready
 
-$("#cerrartodas").click(function() {
-  $(".respuesta").fadeOut(200);
-  $("h3").removeClass("cerrar");
-  $("#cerrartodas").fadeOut("fast");
-});
-
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbxY_U44D0S3VHyvYlTKxDpwsdP2xliioHcufUcEZnxaei-ehnYL/exec";
-const form = document.forms["submit-to-google-sheet"];
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then(document.getElementById("email-conf").removeAttribute("id"))
-    .catch(error => console.error("Error!", error.message));
+$("#closeAll").click(function() {
+  $(".question").fadeOut(200);
+  $("h3").removeClass("closed");
+  $("#closeAll").fadeOut("fast");
 });
